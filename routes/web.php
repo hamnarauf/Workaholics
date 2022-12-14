b78e6eca8caeb495dcecf41417ea6b5bd23fe6f4
Tanzeel Ur Rehman <khawajatanzeel.55221@gmail.com>
2022-12-14 23:58:51 +0500

    webphp error

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,4 +29,3 @@ Route::get('/categories', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
