4890afeb7fe182e9b0e630a492d2cad7f6d3ca2d
333811 <mnawan.bese20seecs@seecs.edu.pk>
2022-12-19 12:24:43 -0700

    gig Detail

---
+++ resources/views/gigDetail/client.blade.php
@@ -0,0 +1,15 @@
+<?php
+$attrs = [
+    "Coutnry" => "Pakistan",
+    "Job Posted" => "3",
+    "Member Since" => "3 Dec 2022"
+];
+
+?>
+
+<x-clientOverview
+   clientName="Your name"
+   clientImg="https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdlYnNpdGV8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"
+   desc="I'm Liam, last year I decided to quit my job and travel the world. You can follow my journey on this blog!"                
+   :attrs=$attrs             
+></x-clientOverview>
