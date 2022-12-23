3d760971c50fa9480c29f7964925d230bc5d83da
Syeda Sana Zehra Zaidi <76579833+sanazehra2001@users.noreply.github.com>
2022-12-24 04:04:50 +0500

    Updated gig.blade

--- resources/views/users/profile/gigs.blade.php
+++ resources/views/users/profile/gigs.blade.php
@@ -1,14 +1,15 @@
 <?php
+
 $skills = array("php", "javasript", "CSS", "Design");
 ?>
 
 @foreach($gigs as $gig)
 <x-list-item-with-image
     title="{{ $gig['name'] }}"
-    personName="Charls Xaviar"
+    personName="{{ $gig['category'] }}"
     personImg="https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdlYnNpdGV8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"
     price="${{ $gig['budget'] }}"
-    date=""
+    date="${{ $gig['required_days'] }}"
     :tags=$skills
     desc="{{ $gig['description'] }}"
     imgSrc="https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdlYnNpdGV8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"
