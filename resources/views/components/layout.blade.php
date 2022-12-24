c7c740d73f201133574f9d5887d9dcbb32762b73
Hamna Rauf <hamnarauf7@gmail.com>
2022-12-24 22:07:38 +0500

    Website Icon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,6 +8,9 @@
     <meta content="" name="description">
     <meta content="" name="keywords">
 
+      <link rel="icon" type="image/x-icon" href="{{asset('img/blue-lamp.png')}}">
+
+
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
     <!-- Vendor CSS Files -->
