5891bcc661cfe1bdfa8dd5ff800f359b314b9e6f
Syeda Sana Zehra Zaidi <76579833+sanazehra2001@users.noreply.github.com>
2022-12-24 13:20:01 +0500

    ...

--- resources/views/users/profile/gigs.blade.php
+++ resources/views/users/profile/gigs.blade.php
@@ -3,18 +3,18 @@
     foreach ( $user['skills'] as $skill) {
         array_push($skill_list, $skill);
     }
+
+    $skill_list = array("a", "v", "c")
 ?>
 
-@foreach ($gigs as $gig)
 <x-list-item-with-image
-    title="Jhand Zindagi"
-    personName="{{ $catList[0] }}"
+    title="A"
+    personName="B"
     personImg="https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdlYnNpdGV8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"
-    price="{{ $gig['budget'] }}"
-    date="{{ $gig['required_days'] }}"
+    price="C"
+    date="D"
     :tags=$skill_list
     desc="Rona aa re la hai bhaiii"
     imgSrc="https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHdlYnNpdGV8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"
 >
 </x-list-item-with-image>
-@endforeach
