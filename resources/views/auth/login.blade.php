b4b349a13122b416fa200cbec3a33aed63bc7b77
Amna Ahmad <74097025+amnaahmad20@users.noreply.github.com>
2022-12-25 11:45:27 -0700

    css login signup

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+<x-layout> 
+    <section>
 
-@section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -70,4 +70,8 @@
         </div>
     </div>
 </div>
-@endsection
+
+
+    </section>
+
+</x-layout>
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
-
-@section('content')
+<x-layout>
+    <section>
+        
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -25,4 +25,7 @@
         </div>
     </div>
 </div>
-@endsection
+
+
+    </section>
+</x-layout>
