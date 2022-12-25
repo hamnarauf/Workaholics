59bf71ac17ee5fa7765a74e6e48d5930ed3ad055
Hamna Rauf <hamnarauf7@gmail.com>
2022-12-25 21:45:27 +0500

    Buttons for education and work

--- resources/views/users/forms/form.blade.php
+++ resources/views/users/forms/form.blade.php
@@ -7,6 +7,10 @@
                     <img class="rounded-circle mt-5" width="150px" src="{{ asset($user['img']) }}">
                     <span class="font-weight-bold">{{ $user['name'] }}</span>
                     <span class="text-black-50">{{ $user['email'] }}</span><span> </span></div>
+                    
+                    <div class="mt-2 text-center"><button class="btn btn-primary profile-button" type="submit">Add Education</button></div>
+                    <div class="mt-1 text-center"><button class="btn btn-primary profile-button" type="submit">Add Employment</button></div>
+
             </div>
             <div class="col-md-5 border-right">
                 <div class="p-3 py-5">
