fbfd761c0086092dfae63eadbcc9911d7dc9ac3c
Hamna Rauf <hamnarauf7@gmail.com>
2022-12-25 18:19:35 +0500

    Milestones styling

--- resources/views/components/milestone.blade.php
+++ resources/views/components/milestone.blade.php
@@ -1,14 +1,12 @@
-<div class="activity-item d-flex">
-    <div class="activite-label">Due {{$dueDate}}</div>
-    <i class='bi bi-circle-fill activity-badge text-danger align-self-start'></i>
+<div class="activity-item d-flex col-md-10 col-12 card p-4 my-2">
     <div class="container">
-        <div class="activity-content title d-inline">{{$title}}</div>
-        <div class="d-inline mx-2">
+        <div class="activity-content title d-inline h4">{{$title}}</div>
+        <div class="d-inline mx-3">
             <x-tagMenu>
                 <x-tag title="{{ $status }}"></x-tag>
             </x-tagMenu>
 
-            <div class="float-right">
+            <div class="float-md-right mr-3">
                 <form action="/milestones" method="POST">
                     @csrf 
                     @method('PUT')
@@ -30,7 +28,10 @@
 
         <ul class="card-meta list-inline">
             <li class="list-inline-item">
-                <i class="ti-calendar"></i> Amount: {{$price}}
+            <i class="ti-money"></i>Amount: {{$price}}
+            </li>
+            <li class="list-inline-item">
+                <i class="ti-calendar"></i> Due: {{$dueDate}}
             </li>
         </ul>
 
--- resources/views/milestones/index.blade.php
+++ resources/views/milestones/index.blade.php
@@ -12,11 +12,13 @@
                             <div class="card-body">
                                 <h4 class="card-title text-center mt-4">Milestones <span>| Timeline</span></h4>
 
-                                <div class="activity">
+                                <div class="activity row d-flex justify-content-center">
                                     @include('milestones.createMilestone.card')
                                 </div>
-                                <div class="mt-10">
-                                    <a href="{{ url('/milestones/create/'. $job_id) }}" class="btn btn-primary mt-10 ">Propose New Milestone</a>
+                                <div class="row d-flex justify-content-center mt-4">
+                                    <div class="mt-10 col-md-10 col-12">
+                                        <a href="{{ url('/milestones/create/'. $job_id) }}" class="btn btn-primary mt-10 ">Propose New Milestone</a>
+                                    </div>
                                 </div>
                             </div>
                         </div><!-- End Recent Activity -->
